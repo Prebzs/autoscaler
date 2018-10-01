94d89c7a704b6b0455134d251df5b4ac25f4d47b
Markus Schuetterle <m.schuetterle@gmx.de>
2018-10-01 17:50:40 +0200

    add function to get coordination relevant information from hpa

--- cluster-autoscaler/hpaConnector/hpa.go
+++ cluster-autoscaler/hpaConnector/hpa.go
@@ -1,18 +1,32 @@
 package hpaConnector
 
 import (
+	"encoding/json"
 	"github.com/golang/glog"
-	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeclient "k8s.io/client-go/kubernetes"
+	"strings"
 )
 
 func WatchHpa(clientSet kubeclient.Interface) {
 
 	hpaWatch, _ := clientSet.AutoscalingV1().HorizontalPodAutoscalers("").Watch(metav1.ListOptions{})
 	data := <-hpaWatch.ResultChan()
+	dataJSON, _ :=json.Marshal(data)
+	dataString := string(dataJSON);
 
-	hpa, _ := meta.NewAccessor().Namespace(data.Object)
-	glog.V(1).Info(hpa);
+	// Seach for DesiredReplicas String
+	DesiredReplicasIndex := strings.Index(dataString, "desiredReplicas")
+	DesiredReplicasIndex=DesiredReplicasIndex+17
+	DesiredReplicaString1:=dataString[DesiredReplicasIndex:DesiredReplicasIndex+3]+""
+
+	//Search for } in String
+	DesiredReplicasKommaIndex := strings.Index(DesiredReplicaString1, "}")
+	DesiredReplicaString := DesiredReplicaString1[0:DesiredReplicasKommaIndex]
+
+	// Log for Debugging
+	glog.V(1).Info(DesiredReplicaString)
+
 
 }
+
